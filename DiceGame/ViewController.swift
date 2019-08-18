549214b28d1470ef5fdc795d6fb16bde9a7af90a
Dionte <Dionte_sb@yahoo.com>
2019-08-17 20:58:34 -0700

    The diceRolled function will generator random numbers to be placed in the dice array.

--- DiceGame/ViewController.swift
+++ DiceGame/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceImage1: UIImageView!
+    @IBOutlet weak var diceImage2: UIImageView!
+    
+    
+    private let dices = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"] // Dice image assets array
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    /*
+     This function will create two random numbers to be generatored,
+     then the two numbers will be put into the dices array to pick out a dice number.
+     */
+    func diceRolled() {
+        
+        // Create two random numbers generators
+        let randomNumber1 = Int.random(in: 0..<6)
+        let randomNumber2 = Int.random(in: 0..<6)
+        
+        diceImage1.image = UIImage(named: dices[randomNumber1])
+        diceImage2.image = UIImage(named: dices[randomNumber2])
+    }
+    
+    
+    @IBAction func rollDiceTapped(_ sender: UIButton) {
+        diceRolled()
+    }
+    
 
 }
 
